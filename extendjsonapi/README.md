b71d7cddde4a40df1bee4951aa9a1552a5e2d3ce
Linzhe He <you@example.com>
2023-09-21 00:03:16 -0700

    extended json api built

---
+++ extendjsonapi/README.md
@@ -0,0 +1,11 @@
+Project purpose: extend the json api to make it support encode and decode for complex and range.
+
+To use the extended json api:
+from extendjsonapi import *
+import json
+
+To use the the encoder:
+json.dumps(obj, cls=MyEncoder)
+
+To use the decoder:
+json.loads(obj, cls=MyDecoder)
---
+++ extendjsonapi/tests/test_extendjsonapi.py
@@ -0,0 +1,24 @@
+from extendjsonapi import *
+import json
+import pytest
+
+
+# test the encoder and decoder for complex
+def test_encoded():
+    test_complex = complex(1, 2)
+
+    encoded_complex = json.dumps(test_complex, cls=MyEncoder)
+    decoded_complex = json.loads(encoded_complex, cls=MyDecoder)
+
+    assert decoded_complex == test_complex
+
+
+# test the encoder and decoder for range
+def test_decoded():
+    test_range = range(1, 10, 3)
+
+    encoded_range = json.dumps(test_range, cls=MyEncoder)
+    decoded_range = json.loads(encoded_range, cls=MyDecoder)
+    
+    assert decoded_range == test_range
+
